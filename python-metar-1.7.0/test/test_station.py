83f84eb67606724f211f760f6a2198c5a93e739c
svp318 <simonvega1990@hotmail.com>
2020-09-10 19:47:19 -0500

    Update gitignore Remove python-metar-1.7.0

--- python-metar-1.7.0/test/test_station.py
+++
@@ -1,8 +0,0 @@
-"""Test metar/Station.py."""
-from metar import Station
-
-
-def test_station():
-    """Can we build a station object."""
-    st = Station.station('KDSM')
-    assert st.id == 'KDSM'
